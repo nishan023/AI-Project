0d3070ccfcebf9947acc8e5d683ebd31f82b9f7c
Prashant <r56.prashant@gmail.com>
2024-07-01 21:52:16 +0545

    bug-fix: fixed google oauth

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,11 +10,11 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId="755974647547-4hm4aj5a4rcvhp6644tnljffve50ttpg.apps.googleusercontent.com">
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <AppRoutes />
-      </PersistGate>
-    </Provider>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <AppRoutes />
+        </PersistGate>
+      </Provider>
     </GoogleOAuthProvider>
   </React.StrictMode>
 );
